0fc05c0cee295aa08d1891be62620f8fa345e6bb
Santanu <santanu7299@gmail.com>
2026-03-18 20:30:40 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -4,3 +4,18 @@ two_sum.py
 # Approach: HashMap (Dictionary)
 # Time Complexity: O(n)
 # Space Complexity: O(n)
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        mp = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in mp:
+                return [mp[complement], i]
+            
+            mp[nums[i]] = i
+        
+        return []
