8a85ed186a791763a8352dd2a528ac75ef15a316
Santanu <santanu7299@gmail.com>
2026-03-22 19:29:20 +0530

    Create maximum_subarray.py

---
+++ maximum_subarray.py
@@ -0,0 +1,15 @@
+# maximum_subarray.py
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum=nums[0]
+        max_sum=nums[0]
+
+        for i in range(1,len(nums)):
+            curr_sum= max(nums[i],curr_sum +nums[i])
+            max_sum =max(max_sum, curr_sum)
+
+        return max_sum
+
+#Complexity
+#Time:O(n)
+#Space : O(1)
