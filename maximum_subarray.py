6d3795e5f612cf606a83ca8968f6e313aa899af5
Santanu <santanu7299@gmail.com>
2026-03-24 19:16:14 +0530

    Update maximum_subarray.py

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -5,7 +5,7 @@ class Solution:
         max_sum=nums[0]
 
         for i in range(1,len(nums)):
-            curr_sum= max(nums[i],curr_sum +nums[i])
+            curr_sum=max(nums[i],curr_sum +nums[i])
             max_sum =max(max_sum, curr_sum)
 
         return max_sum
