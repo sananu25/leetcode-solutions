e91662b51530953d46b81c3ea1919075b8bfb5ba
Santanu <santanu7299@gmail.com>
2026-03-28 19:10:39 +0530

    Create Longest_Common_Prefix.py

---
+++ Longest_Common_Prefix.py
@@ -0,0 +1,18 @@
+# Longest_Common_Prefix.py
+
+
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""      
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i >=len(s) or s[i]!=strs[0][i]:
+                    return res
+            res+=strs[0][i]        
+        return res
+
+
+#Time Complexity:O(n * m)
+#Space Complexity:O(1)
