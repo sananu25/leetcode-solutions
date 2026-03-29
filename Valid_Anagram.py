f8c8ccc94916306943359abde3e955a0f45665a4
Santanu <santanu7299@gmail.com>
2026-03-29 22:12:40 +0530

    Update Valid_Anagram.py

--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -4,7 +4,6 @@ class Solution:
     def isAnagram(self,s: str,t: str) -> bool:
         if len(s)!=len(t):
             return False
-        
         count = {}
         for ch in s:
             count[ch]=count.get(ch,0) +1
