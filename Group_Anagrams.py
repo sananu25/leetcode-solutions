b3d8f2194029aae1d047949bb934d5a4a691ab2c
Santanu <santanu7299@gmail.com>
2026-03-30 19:29:06 +0530

    Update Group_Anagrams.py

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -4,13 +4,13 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = {}
+        groups={}
 
         for word in strs:
-            key = "".join(sorted(word))
+            key ="".join(sorted(word))
             
             if key not in groups:
-                groups[key] = []
+                groups[key]=[]
             
             groups[key].append(word)
         
