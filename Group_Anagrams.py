48292db5bc80f194b263bb3446297cc7a7869cf8
Santanu <santanu7299@gmail.com>
2026-03-30 19:27:46 +0530

    Update Group_Anagrams.py

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -1,2 +1,19 @@
 #  Group_Anagrams.py
 
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+            
+            if key not in groups:
+                groups[key] = []
+            
+            groups[key].append(word)
+        
+        return list(groups.values())
+
+  
